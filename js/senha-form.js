d645624ae474b0efe4c7da15966eed8fae7e6825
mathieuz <matheusandrewww@gmail.com>
2023-02-12 16:50:51 -0300

    12/02/2023

--- js/senha-form.js
+++ js/senha-form.js
@@ -7,15 +7,15 @@ let inputSenha = document.getElementById("inputSenha")
 mostrarSenha.addEventListener("click", () => {
     if (estaLigado === false){
         mostrarSenha.src = "../img/icones/mostrarSenhaOn.png"
+        mostrarSenha.title = "Ocultar Senha"
         inputSenha.type = "text"
-        inputSenha.title = "Mostrar Senha"
 
         estaLigado = true
 
     } else if (estaLigado === true) {
         mostrarSenha.src = "../img/icones/mostrarSenhaOff.png"
+        mostrarSenha.title = "Mostrar Senha"
         inputSenha.type = "password"
-        inputSenha.title = "Ocultar Senha"
 
         estaLigado = false
     }
