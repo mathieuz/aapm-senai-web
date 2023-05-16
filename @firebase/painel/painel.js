add3e254b17f2a9f6e39e0f01f68f6beb7e46cb1
mathieuz <matheusandrewww@gmail.com>
2023-05-16 10:27:41 -0300

    [!] Dark Mode

--- @firebase/painel/painel.js
+++ @firebase/painel/painel.js
@@ -1,30 +1,63 @@
 /* Importando variáveis necessárias do arquivo module. */
 import { ath, db } from "../module.js"
 
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+import { doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
 /* Importação dos módulos de conexão do Firebase. */
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js"
 
 let modalPerfil = document.getElementById("modalPerfil")
 
+let hrefCss = document.head.getElementsByTagName("link")[0]
+let logoSenai = document.getElementById("senaiLogo")
+let imgIconAdmin = document.getElementById("imgIconAdmin")
+let imgAdmin = document.getElementById("imgAdmin")
 
 const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, async (user) => {
   if (user) {
+    const adminDoc = doc(db, "Administrador", user.email)
+    const admin = await getDoc(adminDoc)
+
+    if (admin.get("darkMode") == true){
+      hrefCss.href = "css/painel/painel-dm.css"
+      logoSenai.src = "../../img/senai-logo-branco.png"
+      imgIconAdmin.src = "../../img/icones/icon-foto-perfil-dm.png"
+      imgAdmin.src = "../../img/icones/icon-foto-perfil-dm.png"
+
+    } else if (admin.get("darkMode") == false){
+      hrefCss.href = "css/painel/painel.css"
+      logoSenai.src = "../../img/senai-logo.png"
+      imgIconAdmin.src = "../../img/icones/icon-foto-perfil.png"
+      imgAdmin.src = "../../img/icones/icon-foto-perfil.png"
+    }
 
     //Adiciona função dark/light mode.
-    document.getElementById("btnDarkLightMode").onclick = () => {
-      let hrefCss = document.head.getElementsByTagName("link")[0]
-      let logoSenai = document.getElementById("senaiLogo")
+    document.getElementById("btnDarkLightMode").onclick = async () => {
 
       if (hrefCss.href.includes("css/painel/painel.css")){
         hrefCss.href = "css/painel/painel-dm.css"
-        logoSenai.src = "img/senai-logo-branco.png"
+        logoSenai.src = "../../img/senai-logo-branco.png"
+        imgIconAdmin.src = "../../img/icones/icon-foto-perfil-dm.png"
+        imgAdmin.src = "../../img/icones/icon-foto-perfil-dm.png"
 
-      } else {
+        const alterarDarkMode = doc(db, "Administrador", user.email)
+        await updateDoc(alterarDarkMode, {
+          darkMode: true
+        })
+
+
+      } else if (hrefCss.href.includes("css/painel/painel-dm.css")){
         hrefCss.href = "css/painel/painel.css"
-        logoSenai.src = "img/senai-logo.png"
+        logoSenai.src = "../../img/senai-logo.png"
+        imgIconAdmin.src = "../../img/icones/icon-foto-perfil.png"
+        imgAdmin.src = "../../img/icones/icon-foto-perfil.png"
+
+        const alterarDarkMode = doc(db, "Administrador", user.email)
+        await updateDoc(alterarDarkMode, {
+          darkMode: false
+        })
+
       }
     }
 
