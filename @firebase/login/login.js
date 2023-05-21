5488ec895958e35951da4d240a6fce88efec436d
mathieuz <matheusandrewww@gmail.com>
2023-05-21 13:47:08 -0300

    [!] Adicionado Pop-up

--- @firebase/login/login.js
+++ @firebase/login/login.js
@@ -6,16 +6,33 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } f
 
 import { doc, getDoc, setDoc, addDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
+/*Pop-up: 0 > Alerta, 1 > Sucesso.*/
+let popUp = document.getElementsByClassName("popUpAvisos")
+let spanPopUp = document.getElementsByClassName("spanPopUp")
+
 //Função que realiza o login na plataforma.
 document.getElementById("btnFazerLogin").addEventListener("click", () => {
     let email = document.getElementById("inputEmail").value
     let senha = document.getElementById("inputSenha").value
 
+    if (String(email).length == 0 || String(email).length == 0){
+        spanPopUp[0].innerHTML = "Um ou mais campos estão vazios. Preencha todos campos e tente novamente."
+        popUp[0].style.display = "flex"
+
+        setTimeout(() => {
+            popUp[0].style.display = "none"
+        }, 3000)    
+    }
+
     const autenticacao = getAuth()
     signInWithEmailAndPassword(autenticacao, email, senha)
         .then((userCredential) => {
-            alert("[SUCESSO] E-mail autenticado com sucesso.")
-            window.location.href = "./painel.html"
+            spanPopUp[1].innerHTML = "Autenticação realizada com sucesso. Redirecionando..."
+            popUp[1].style.display = "flex"
+
+            setTimeout(() => {
+                window.location.href = "./painel.html"
+            }, 2000)
 
         })
         .catch( async (error) => {
@@ -24,21 +41,39 @@ document.getElementById("btnFazerLogin").addEventListener("click", () => {
 
             //Se a conta dada ao realizar ao login existir, faz o login.
             if (docSnap.exists()){
-                if (senha === atob(docSnap.get("senhaAdmin"))){
-                    createUserWithEmailAndPassword(autenticacao, email, senha)
-                        .then(async (userCredential) => {
-                            alert("[SUCESSO] E-mail autenticado com sucesso.")
-                            window.location.href = "./painel.html"
-                        })
-                        .catch((error) => {
-                            alert(error)
-                        });
+                try{
+                    if (senha === atob(docSnap.get("senhaAdmin"))){
+                        createUserWithEmailAndPassword(autenticacao, email, senha)
+                            .then(async (userCredential) => {
 
-                } else {
-                    alert("[ERRO] Senha incorreta. Tente novamente.")
-                }
-            } else {
-                alert("[ERRO] A conta informada não existe no sistema.")
+                                spanPopUp[1].innerHTML = "Autenticação realizada com sucesso. Redirecionando..."
+                                popUp[1].style.display = "flex"
+                
+                                setTimeout(() => {
+                                    window.location.href = "./painel.html"
+                                }, 3000)
+
+                            })
+                            .catch((error) => {
+                                alert(error)
+                            });
+
+                    } else {
+                        spanPopUp[0].innerHTML = "Senha ou e-mail incorretos. Verifique os campos e tente novamente."
+                        popUp[0].style.display = "flex"
+
+                        setTimeout(() => {
+                            popUp[0].style.display = "none"
+                        }, 3000)
+                    }
+                } catch {}
             }
+
+            spanPopUp[0].innerHTML = "Senha ou e-mail incorretos. Verifique os campos e tente novamente."
+            popUp[0].style.display = "flex"
+
+            setTimeout(() => {
+                popUp[0].style.display = "none"
+            }, 3000)
         });
 })
