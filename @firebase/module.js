4afa7312d5ec18c3064c99dc362961b1f7f1008c
mathieuz <81052061+mathieuz@users.noreply.github.com>
2023-06-14 10:55:21 -0300

    Update module.js

--- @firebase/module.js
+++ @firebase/module.js
@@ -12,12 +12,12 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-s
 /* Connfirguração de conexão entre a aplicação e o Firebase. */
 const firebaseConfig = {
     apiKey: "AIzaSyDM0ahJj0alvS4TAXQbdhXs5GaTbvKs2Dg",
-    authDomain: "senai-tcc-aapm.firebaseapp.com",
-    projectId: "senai-tcc-aapm",
-    storageBucket: "senai-tcc-aapm.appspot.com",
-    messagingSenderId: "522581165702",
-    appId: "1:522581165702:web:b891574b76e52a4272b307",
-    measurementId: "G-GMJB0HWNLP"
+  authDomain: "senai-tcc-aapm.firebaseapp.com",
+  projectId: "senai-tcc-aapm",
+  storageBucket: "senai-tcc-aapm.appspot.com",
+  messagingSenderId: "522581165702",
+  appId: "1:522581165702:web:b891574b76e52a4272b307",
+  measurementId: "G-GMJB0HWNLP"
 };
 
 /* A constante 'app' tem o método que inicializa a conexão do Firebase, além de ser utilizado como parâmetro de inicialização de módulos de conexão do Firebase. */
